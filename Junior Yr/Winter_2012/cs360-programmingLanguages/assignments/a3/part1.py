d152cc8ae7a390d3fee481ece433ed829365f426
Sunny Patel <sunny@sunny-ThinkPad-T410.(none)>
2013-02-13 09:33:17 -0500

    working on cs360 assignment

--- Junior Yr/Winter_2012/cs360-programmingLanguages/assignments/a3/part1.py
+++ Junior Yr/Winter_2012/cs360-programmingLanguages/assignments/a3/part1.py
@@ -1,8 +1,36 @@
 input_dir = "inputs/"
-file = open(input_dir,'r')
+f = open(input_dir + "sample1.input",'r')
 
-while 1:
-	line = file.readline()
-	if not line:
-		break
-	pass
+numOfStates = f.readline()
+print "Number of states = " + numOfStates
+
+f_dot = open("out.dot","w");
+# write defaults
+f_dot.write("digraph fsm {\nrankdir=\"LR\"\nstart [shape=\"plaintext\" ,label=\"start\"]\n")
+
+# write the states dot code
+for i in range(int(numOfStates)-1):
+	f_dot.write(str(i) + " [shape=\"circle\",label=\"S" + str(i) + "\"]\n")
+lastState = str(int(numOfStates)-1)
+# write the last end state which uses 'doublecircle'
+f_dot.write(lastState + " [shape=\"doublecircle\",label=\"S" + lastState + "\"]\n")
+
+
+# *** BEGIN WRITTING TRANSITIONS ***
+# move f line pointer nxt line, ignoring 2nd line
+numOfTransitions = f.readline()
+
+# write which is the start state
+f_dot.write("start->1\n")
+
+
+inputs = []
+for i in range(int(numOfTransitions)):
+	inputs.append(f.readline())
+print "Number of elements as inputs[]=" + str(len(inputs))
+
+# split each element of input by token ','
+for state in inputs:
+	state_data = state.split(',')
+	
+f.close()
