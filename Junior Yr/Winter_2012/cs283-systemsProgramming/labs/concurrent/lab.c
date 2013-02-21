c27eab9005ba2314869a6cc8b52ee22c8cd335b1
Sunny Patel <du.svp28@gmail.com>
2013-02-21 13:06:01 -0500

    Finalized cs283 lab concurrancy.  Submitting now

--- Junior Yr/Winter_2012/cs283-systemsProgramming/labs/concurrent/lab.c
+++ Junior Yr/Winter_2012/cs283-systemsProgramming/labs/concurrent/lab.c
@@ -2,61 +2,65 @@
 
 // struct to pass args to thread 
 typedef struct{
-		int *a;
-		pthread_mutex_t *mutex;
+	int *a;
+	pthread_mutex_t *mutex;
 } args;
 
 /* Thread function to add 1 to &count 1000 times each */
 void *thread(void *arg){
-		int *a = ((args*)arg)->a;
-		int i;
-		// being looping
-		for(i=0; i < 1000; i++) {
-				(*a)++;
-		}
-		pthread_exit(0);
+	int *a = ((args*)arg)->a;
+	int i;
+	// being looping
+	for(i=0; i < 1000; i++) {
+		(*a)++;
+	}
+	pthread_exit(0);
 }
 
 int main(){
-        // init clock vars
-		clock_t begin, end;
-        double time_spent;
-		// saved time program started
-        begin = clock();
+	// init clock vars
+	clock_t begin, end;
+	double time_spent;
+	// saved time program started
+	begin = clock();
 
-		// shared var to be summed
-		int count = 0;	
-		int numOfThreads = 1000;
-		// create a list of thread ids 
-		pthread_t tid[numOfThreads];
-		// create mutex lock
-		pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;	
-		// prepare thread args
-		args args1;	
-		args1.a = &count;
-		args1.mutex = &mutex;
+	// shared var to be summed
+	int count = 0;	
+	int numOfThreads = 1000;
+	// create a list of thread ids 
+	pthread_t tid[numOfThreads];
+	// create mutex lock
+	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;	
+	// prepare thread args
+	args args1;	
+	args1.a = &count;
+	args1.mutex = &mutex;
 
-		int threadCount = 0;
-		// loop to create the threads
-		for(threadCount = 0; threadCount < numOfThreads; threadCount++){
-				Pthread_create(&tid[threadCount], NULL, thread, &args1);
-		}
-		printf("Threads created: %d\n", threadCount);		
+	int threadCount = 0;
+	// loop to create the threads
+	for(threadCount = 0; threadCount < numOfThreads; threadCount++){
+		Pthread_create(&tid[threadCount], NULL, thread, &args1);
+	}
 
-		// loop to join the threads to main, prevents main from exiting until all the 
-		// threads are finished executing
-		int joinCount;
-		for(joinCount = 0; joinCount < threadCount; joinCount++){
-				Pthread_join(tid[joinCount],NULL);	
-		}
+	// loop to join the threads to main, prevents main from exiting until all the 
+	// threads are finished executing
+	int joinCount;
+	for(joinCount = 0; joinCount < threadCount; joinCount++){
+		Pthread_join(tid[joinCount],NULL);	
+	}
 
-		printf("count= %d\n",count);
 
-		// work finished, get clock time
-        end = clock();
-		// calculate how much time was spent
-        time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-        printf("Time = %f\n",time_spent);
-		// exit main thread
-		pthread_exit(0);
+	// work finished, get clock time
+	end = clock();
+	// calculate how much time was spent
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("*********************\n");
+	printf("*     No Locks      *\n");
+	printf("*********************\n");
+
+	printf("Threads created: %d\n", threadCount);   
+	printf("count= %d\n",count);
+	printf("Time = %f\n",time_spent);
+	// exit main thread
+	pthread_exit(0);
 }
--- Junior Yr/Winter_2012/cs283-systemsProgramming/labs/concurrent/lab_wMutex_outside.c
+++ Junior Yr/Winter_2012/cs283-systemsProgramming/labs/concurrent/lab_wMutex_outside.c
@@ -1,62 +1,66 @@
 #include "csapp.h"
 
 typedef struct{
-		int *a;
-		pthread_mutex_t *mutex;
+	int *a;
+	pthread_mutex_t *mutex;
 } args;
 
 void *thread(void *arg){
-		int *a = ((args*)arg)->a;
-		pthread_mutex_t *mutex = ((args*)arg)->mutex;
-		int i;
-		// mutex lock here
-		pthread_mutex_lock(mutex);
-		// being looping
-		for(i=0; i < 1000; i++) {
-				(*a)++;
-		}
-		// remove lock
-		pthread_mutex_unlock(mutex);
-		pthread_exit(0);
+	int *a = ((args*)arg)->a;
+	pthread_mutex_t *mutex = ((args*)arg)->mutex;
+	int i;
+	// mutex lock here
+	pthread_mutex_lock(mutex);
+	// being looping
+	for(i=0; i < 1000; i++) {
+		(*a)++;
+	}
+	// remove lock
+	pthread_mutex_unlock(mutex);
+	pthread_exit(0);
 }
 
 int main(){
-        clock_t begin, end;
-        double time_spent;
-        begin = clock();
+	clock_t begin, end;
+	double time_spent;
+	begin = clock();
 
-		// shared var to be summed
-		int count = 0;	
-		int numOfThreads = 1000;
+	// shared var to be summed
+	int count = 0;	
+	int numOfThreads = 1000;
 
-		pthread_t tid[numOfThreads];
-		// prepare thread args
-		pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;	
+	pthread_t tid[numOfThreads];
+	// prepare thread args
+	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;	
 
-		args args1;	
-		args1.a = &count;
-		args1.mutex = &mutex;
+	args args1;	
+	args1.a = &count;
+	args1.mutex = &mutex;
 
-		int threadCount = 0;
-		// loop to create the threads
-		for(threadCount = 0; threadCount < numOfThreads; threadCount++){
-				Pthread_create(&tid[threadCount], NULL, thread, &args1);
-		}
-		printf("Threads created: %d\n", threadCount);		
+	int threadCount = 0;
+	// loop to create the threads
+	for(threadCount = 0; threadCount < numOfThreads; threadCount++){
+		Pthread_create(&tid[threadCount], NULL, thread, &args1);
+	}
 
-		// loop to join the threads to main, prevents main from exiting until all the 
-		// threads are finished executing
-		int joinCount;
-		for(joinCount = 0; joinCount < threadCount; joinCount++){
-				Pthread_join(tid[joinCount],NULL);	
-		}
-
-		printf("count= %d\n",count);
+	// loop to join the threads to main, prevents main from exiting until all the 
+	// threads are finished executing
+	int joinCount;
+	for(joinCount = 0; joinCount < threadCount; joinCount++){
+		Pthread_join(tid[joinCount],NULL);	
+	}
 
 
-        end = clock();
-        time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-        printf("Time = %f\n",time_spent);
+	end = clock();
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-		pthread_exit(0);
+	printf("*********************\n");
+	printf("* Lock Outside for  *\n");
+	printf("*********************\n");
+
+	printf("Threads created: %d\n", threadCount);   
+	printf("count= %d\n",count);
+	printf("Time = %f\n",time_spent);
+
+	pthread_exit(0);
 }
