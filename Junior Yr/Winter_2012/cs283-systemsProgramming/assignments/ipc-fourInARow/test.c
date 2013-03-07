de9ccdf59c0252f36cd6df24b87c9be963a9aa9f
Sunny Patel <sunny@ubuntu.(none)>
2013-03-07 03:15:17 -0500

    checked all syntax now adding parent body

--- Junior Yr/Winter_2012/cs283-systemsProgramming/assignments/ipc-fourInARow/test.c
+++ Junior Yr/Winter_2012/cs283-systemsProgramming/assignments/ipc-fourInARow/test.c
@@ -1,23 +1,24 @@
 #include "csapp.h"
+
 typedef struct{
 	int board[10][10]; // a 8x8 gameboard, we use 10x10 array to avoid out of bounds error
 	int playerTurn; // represents whose turn it is next
-	int winner = 0;     // holds the PID of winning process.  hopefully of the parent
-	int gameover = 0;   // is the gameover? Just in case there are no possible moves left, in which case game would be considered a tie
+	int winner;     // holds the PID of winning process.  hopefully of the parent
+	int gameover;   // is the gameover? Just in case there are no possible moves left, in which case game would be considered a tie
 } game;
 
 typedef struct{
-	int i = -1; 
-	int j = -1;
+	int i; 
+	int j;
 } position;
 
 // check in all directions around the position given and returns if there is
 // a line in the given direction, 
 // if direction is -1 then checks all direction
-int checkAround(int *board[10][10], position pos, int *currentDirection, int *lineCount);
+int checkAround(int board[10][10], position *pos, int *currentDirection, int *lineCount);
 
 // checks if there is line
-int checkPosLine(int *board[10][10], position *pos, int *currentDirection,	position *newPos);
+int checkPosLine(int board[10][10], position *pos, int *currentDirection,	position *newPos);
 
 // game logic
 int gameLogic(game *g1);
@@ -46,7 +47,7 @@ int main(){
 			position availableMoves[8];
 
 			int i,j;
-			for(i;=1; i<=8; i++){
+			for(i=1; i<=8; i++){
 				int skip=0;
 				for(j=1;(j<=8 && skip==0); j++){
 					if(g1.board[i][j] != 0){
@@ -57,7 +58,10 @@ int main(){
 						availableMoves[i] = p1;
 
 					} else{
-						availableMove[i] = -1;
+						position p1;
+						p1.i = -1;
+						p1.j = -1;
+						availableMoves[i] = p1;
 						skip =1;
 					}
 
@@ -67,7 +71,7 @@ int main(){
 			// randomly pick a position
 			int moved = 0;
 			while(!moved){
-				int f = rand() % 9;
+				int r = rand() % 9;
 				if(isValidPos(availableMoves[r])){
 					// make the move
 					g1.board[availableMoves[r].i][availableMoves[r].j] = 2;
@@ -83,8 +87,8 @@ int main(){
 			int nbytes;
 			int readi;
 			nbytes = read( fd[i][0], &g1, sizeof(g1));
-			sprintf(childString, "%d", readi);
-			printf("Child ;  %s\n", childString);
+			//sprintf(childString, "%d", readi);
+			//printf("Child ;  %s\n", childString);
 		}
 		else {
 			// parent
@@ -108,40 +112,40 @@ int main(){
 	return(0);
 }
 
-gameLogic(game g1){
+int gameLogic(game *g1){
 	int highest, skip = 0;
 	position bestNextMove;
 
 	int i,j;
 	for(i=0; i<=8;i++){
 		for(j=0; (j<=8 && skip == 0); j++){
-			if(g1.board[i][j] == 0){
+			if(g1->board[i][j] == 0){
 				highest =0;
 				int *lineCount;
 				lineCount = 0;
 				// create a new position struct for this spot
 				position *p1 = Malloc(sizeof(position));
-				p1.i = i;
-				p1.j = j;
-				int *currentDirection;
-				currentDirection = -1;
-				if( checkAround(g1.board, p1, *currentDirection, *lineCount) ) { // ********* SUNNY LOOK HERE REWRTE THIS 
-					if(lineCount == 3){ // this is deff one of the best moves available
-						g1.board[i][j] = 1;
-						g1.winner = getPid() // ***** SEMI CODE
-							skip = 1;
+				p1->i = i;
+				p1->j = j;
+				int *currentDirection = Malloc(sizeof(int));
+				*currentDirection = -1;
+				if( checkAround(g1->board, p1, currentDirection, lineCount) ) { // ********* SUNNY LOOK HERE REWRTE THIS 
+					if(*lineCount == 3){ // this is deff one of the best moves available
+						g1->board[i][j] = 1;
+						g1->winner = 1; // ***** SEMI CODE supposed to be PID
+						skip = 1;
 					} // lineCount if
-					else if(lineCount > highest){
+					else if(*lineCount > highest){
 						bestNextMove.i = i;
 						bestNextMove.j = j;
 						skip = 1;
-						highest = lineCount;
+						highest = *lineCount;
 					} // else if(lineCount > highest)
 				} // checkAround
 			} // g1.board[i][j] if
 		} // 2nd for
 		if( highest > 0 ){ // we have found a move
-			g1.board[bestNextMove.i][bestNextMove.j] = 1;
+			g1->board[bestNextMove.i][bestNextMove.j] = 1;
 			return 1;
 		} // highest > 0
 		else {
@@ -151,88 +155,89 @@ gameLogic(game g1){
 }
 
 
-int checkAround(int *board[10][10], position *pos, int *currentDirection,	int *lineCount){
+int checkAround(int board[10][10], position *pos, int *currentDirection,	int *lineCount){
 	int direction = 0;
 	for(direction = 0; direction <=7; direction++){
-		if(currentDirection != direction || currentDirect == -1){
+		if(*currentDirection != direction || *currentDirection == -1){
 			position *newPos = Malloc(sizeof(position));
-			if((int d=checkPosLine(board, pos, direction, newPos)) >= 0){
+			int d;
+			if((d = checkPosLine(board, pos, &direction, newPos)) >= 0){
 				// recrusively loop to check for any other matches
-				checkAround(board, newPos, d, lineCount++);
+				checkAround(board, newPos, &d, lineCount++);
 			} else {
 				if(lineCount > 0)
-					return true;
+					return 1;
 				else 
-					return false;
+					return 0;
 			}
 		}
 	}
 }
 
 
-int checkPosLine(int *board[10][10], position *pos, int *currentDirection, position *newPos){
-	int i = pos.i;
-	int j = pos.j;
+int checkPosLine(int board[10][10], position *pos, int *currentDirection, position *newPos){
+	int i = pos->i;
+	int j = pos->j;
 
-	switch(currentDirection){
+	switch(*currentDirection){
 		// chk top
 		case 0:
 			// another match was found in the direction we're looking
 			if(board[i][j-1] == 1){
-				newPos.i = i;
-				newPos.j = j-1;
+				newPos->i = i;
+				newPos->j = j-1;
 				return 0;
 			}
 			break;
 		case 1:
 			if(board[i+1][j-1] == 1){
-				newPos.i = i+1;
-				newPos.j = j-1;
+				newPos->i = i+1;
+				newPos->j = j-1;
 				return 1;
 			}		
 			break;
 		case 2:
 			if(board[i+1][j] == 1){
-				newPos.i = i+1;
-				newPos.j = j;
+				newPos->i = i+1;
+				newPos->j = j;
 				return 2;
 			}
 			break;
 		case 3:
 			if(board[i+1][j+1] == 1){
-				newPos.i = i+1;
-				newPos.j = j+1;
+				newPos->i = i+1;
+				newPos->j = j+1;
 				return 3;
 			}
 			break;
 
 		case 4:
 			if(board[i][j+1] == 1){
-				newPos.i = i;
-				newPos.j = j+1;
+				newPos->i = i;
+				newPos->j = j+1;
 				return 4;
 			}
-			break 4;
+			break;
 		case 5:
 			if(board[i-1][j+1] == 1){
-				newPos.i = i-1;
-				newPos.j = j+1;
+				newPos->i = i-1;
+				newPos->j = j+1;
 				return 5;
 			}
 			break;
 
 		case 6:
 			if(board[i-1][j] == 1){
-				newPos.i = i-1;
-				newPos.j = j;
+				newPos->i = i-1;
+				newPos->j = j;
 				return 6;
 			}
 			break;
 
 		case 7:
 			if(board[i-1][j-1] == 1){
-				newPos.i = i-1;
-				newPos.j = j-1;
+				newPos->i = i-1;
+				newPos->j = j-1;
 			}
 			break;
 
@@ -243,7 +248,7 @@ int checkPosLine(int *board[10][10], position *pos, int *currentDirection, posit
 
 game createGame(){
 	game g1;
-	int i;
+	int i,j,k,w;
 
 	for(i=0;i<10;i++){
 		g1.board[i][0] = -1;
