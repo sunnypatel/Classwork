6801bb67497832d589357f2643353390fb7e81f8
Sunny Patel <du.svp28@gmail.com>
2013-04-30 12:41:39 -0400

    started overall menus and main development

--- Junior Yr/Spring_2012/cs350-SoftwareDesign/survey/src/Main.java
+++ Junior Yr/Spring_2012/cs350-SoftwareDesign/survey/src/Main.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.ArrayList;
+
 
 
 public class Main {
@@ -6,22 +9,242 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//ArrayList<Question> questions = new ArrayList<Question>();
-		//questions.add(new Matching());
-		//questions.add(new Ranking());
+		Main main = new Main();
+		main.mainMenu();
 		
-		
-		Survey survey1 = new Survey();
-		
-		survey1.createSurvey();
-		survey1.addQuestion();
-		/*
-		//String roman = RomanConversion.binaryToRoman(3);
-		//System.out.println(roman);
-		Matching a = new Matching();
-		String d = a.convertToRoman(3);
-		System.out.println(d); */
 	}
+	
+	public void mainMenu(){
+		surveyMainMenu();
+	}
+	
+	public void surveyMainMenu() {
+		Creader rd = new Creader();
+		boolean done = false;
+		
+		System.out.println("");
+		System.out.println("-- Main Menu: Survey --");
+		do {
+			System.out.println("");
+			System.out.println(" (1) Create new survey");
+			System.out.println(" (2) Load survey");
+			System.out.println(" ---------------------");
+			System.out.println(" (3) Back");
+			// get user input
+			String option = rd.readLine();
+			// make sure user has inputed something
+			if (option.length() > 0) {
 
+				System.out.println("You selected option " + option);
+
+				switch (Integer.parseInt(option)) {
+				case 1: // Create new survey
+					createSurvey();
+					break;
+				case 2: // load survey
+					loadSurvey();
+					break;
+				case 3: // Go back
+					mainMenu();
+					break;
+				default:
+					System.out.println("Sorry, input not recognized. Please try again.");
+					done = false;
+				}
+			} else {
+				done = false;
+				System.out.println("Sorry, input not recognized. Please try again.");
+			}
+		} while (!done);
+	}
+	
+	public void editSurveyMenu(Survey survey) {
+		Creader rd = new Creader();
+		boolean addAnother = true;
+		int questionCount = 0;
+
+		while (addAnother) {
+			boolean errors = false;
+
+			// What type of question are we creating?
+			System.out.println("");
+			System.out.println("-- Survey: " + survey.getName() + " --");
+			System.out.println("To add a question, select a type of question ");
+			System.out.println("from the following options or select Done when finished.");
+
+			System.out.println(" (1) Multiple Choice");
+			System.out.println(" (2) True or False");
+			System.out.println(" (3) Matching");
+			System.out.println(" (4) Ranking");
+			System.out.println(" (5) Essay");
+			System.out.println(" (6) Short Answer");
+			System.out.println(" --------------------");
+			System.out.println(" (7) List all questions");
+			System.out.println(" (8) Save & Close");
+
+			// get user input
+			String option = rd.readLine();
+
+			// make sure user has inputed something
+			if (option.length() > 0) {
+
+				System.out.println("You selected option " + option);
+
+				switch (Integer.parseInt(option)) {
+				case 1: // Multiple Choice
+					survey.addQuestion(new MultipleChoice(1));
+					break;
+				case 2: // True / False
+					survey.addQuestion(new TrueFalse());
+					break;
+				case 3: // Matching
+					Matching matching = new Matching();
+					matching.create(); // <----- DONT LIKE THIS CHANGE IF TIME
+					survey.addQuestion(matching);
+					break;
+				case 4: // Ranking
+					Ranking ranking = new Ranking();
+					survey.addQuestion(ranking);
+					break;
+				case 5: // Essay
+					survey.addQuestion(new Essay());
+					break;
+				case 6: // Short Answer
+					survey.addQuestion(new ShortAns());
+					break;
+				case 7: // List all questions
+					survey.displayQuestions();
+					break;
+				case 8:
+					addAnother = false;
+					System.out.print("Saving...");
+
+					try {
+						survey.save();
+						System.out.println("Done.");
+					} catch (IOException e) {
+						System.out.println("Error.");
+						//e.printStackTrace();
+						errors = true;
+					}
+
+					break;
+				default:
+					System.out.println("Sorry, input not recognized. Please try again.");
+					System.out.println("No question added");
+					System.out.println("");
+					errors = true;
+				}
+			} else {
+				errors = true;
+				System.out
+						.println("Sorry, input not recognized. Please try again.");
+				System.out.println("No question added");
+				System.out.println("");
+			}
+
+			if (!errors && (addAnother != false)) {
+				questionCount = questionCount + 1;
+				System.out.println("Question #" + questionCount
+						+ " was created successfully.");
+
+				System.out.println("Question as reader would see it: ");
+				System.out.println("");
+				System.out.println("-- "
+						+ survey.getQuestions().get(survey.getQuestions().size() - 1).getQuestionType()
+						+ " --");
+				System.out.println("");
+				// ****(MULTI-THREADING WARNING) LINE WILL NOT BE CORRECT IN
+				// CASE OF A RACE CONDITION****
+				survey.getQuestions().get(survey.getQuestions().size() - 1).displayQuestion();
+
+			}
+
+		} // while(addAnother)
+		surveyMainMenu();
+	} // createSurvey()
+	
+	
+	public void createSurvey() {
+		Creader rd = new Creader();
+		Survey survey = new Survey();
+		
+		System.out.println("");
+		System.out.println("-- Menu: Create new survey --");
+		System.out.print("Survey name:");
+		survey.setName(rd.readLine());
+		
+		editSurveyMenu(survey);
+	}
+	
+	public void loadSurvey(){
+		Creader rd = new Creader();
+		boolean done = false;
+		System.out.println("");
+		System.out.println("-- Menu: Load survey --");
+		System.out.println("Options:");
+		System.out.println("type \\list    to view all available surveys)");
+		System.out.println("type \\back    to return to previous menu)");
+		do {
+			System.out.println("");
+			System.out.println("Enter survey name to load:");
+
+			// get user input
+			String option = rd.readLine();
+			// make sure user has inputed something
+			if (option.length() > 0) {
+				if(option.equals("\\list")){
+					// user wants all the surveys listed
+					listSurveys();
+				} else if(option.equals("\\back")){
+					// user wants to go back to survey main menu
+					surveyMainMenu();
+				}
+				else{ // load survey
+					Survey survey = new Survey();
+					// option is the survey name
+					try {
+						survey.load(option);
+					} catch (FileNotFoundException e) {
+						done = false;
+						System.out.println("File not found. Please try again.");
+						//e.printStackTrace();
+					}
+				}
+			} else {
+				done = false;
+				System.out.println("Sorry, input not recognized. Please try again.");
+			}
+		} while (!done);
+	}
+	
+	public void listSurveys(){
+		ArrayList<String> surveys = listFiles("saves/survey/");
+		int count =0;
+		for (String filename : surveys){
+			count++;
+			System.out.println(" "+count+". "+ filename );
+		}
+	}
+	
+	public ArrayList<String> listFiles(String path){
+		ArrayList<String> fileList = new ArrayList<String>();
+		  String files;
+		  File folder = new File(path);
+		  File[] listOfFiles = folder.listFiles(); 
+		 
+		  for (int i = 0; i < listOfFiles.length; i++) 
+		  {
+		 
+		   if (listOfFiles[i].isFile()) 
+		   {
+		   files = listOfFiles[i].getName();
+		       if (files.endsWith(".q") || files.endsWith(".a"))
+		       {
+		          fileList.add(files);
+		        }
+		     }
+		  }
+		  return fileList;
+	}
 }
--- Junior Yr/Spring_2012/cs350-SoftwareDesign/survey/src/Survey.java
+++ Junior Yr/Spring_2012/cs350-SoftwareDesign/survey/src/Survey.java
@@ -1,163 +1,99 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.ArrayList;
 
+public class Survey {
 
-public class Survey implements Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -1516345659842813782L;
 	private String name;
 	private ArrayList<Question> questions;
-	
-	
-	public Survey(){
-		
+
+	public Survey() {
+
 		questions = new ArrayList<Question>();
 	}
-	
-	
-	public void createSurvey(){
-		Creader rd = new Creader();
-		
-		System.out.println("-- Create new survey --");
-		System.out.print("Survey name:");
-		this.setName(rd.readLine());
-	}
-	
-	
-	public void addQuestion(){
-		Creader rd = new Creader();
-		boolean addAnother = true;
-		int questionCount = 0;
-		
-		while(addAnother){
-			boolean errors = false;
-			
-			// What type of question are we creating? 
-			System.out.println("");
-			System.out.println("To add a question, select a type of question ");
-			System.out.println("from the following options or select Done when finished.");
 
-			System.out.println(" (1) Multiple Choice");
-			System.out.println(" (2) True or False");
-			System.out.println(" (3) Matching");
-			System.out.println(" (4) Ranking");
-			System.out.println(" (5) Essay");
-			System.out.println(" (6) Short Answer");
-			System.out.println(" --------------------");
-			System.out.println(" (7) Done");
-			
-			// get user input
-			String option = rd.readLine();	
-			
-			// make sure user has inputed something
-			if(option.length() > 0){
-			
-			
-			System.out.println("You selected option " + option);
-			
-
-				switch(Integer.parseInt(option)){
-				case 1:									// Multiple Choice
-					questions.add(new MultipleChoice(1));
-					break;
-				case 2:									// True / False
-					questions.add(new TrueFalse());
-					break;
-				case 3:									// Matching
-					Matching matching = new Matching();
-					matching.create();				//    <----- DONT LIKE THIS CHANGE IF TIME
-					questions.add(matching);
-					break;
-				case 4:									// Ranking
-					Ranking ranking = new Ranking();
-					questions.add(ranking);
-					break;
-				case 5:									// Essay
-					questions.add(new Essay());
-					break;
-				case 6:									// Short Answer
-					questions.add(new ShortAns());
-					break;
-				case 7:
-					addAnother = false;
-					System.out.print("Saving...");
-					
-					try {
-						this.save();
-						System.out.println("Done.");
-					} catch (IOException e) {
-						System.out.println("Error.");
-						e.printStackTrace();
-					}
-					
-					break;
-				default:
-					System.out.println("Sorry, input not recognized. Please try again.");
-					System.out.println("No question added");
-					System.out.println("");
-					errors = true;
-				}
-			}
-			else {
-				errors = true;
-				System.out.println("Sorry, input not recognized. Please try again.");
-				System.out.println("No question added");
-				System.out.println("");
-			}
-			
-			if (!errors && (addAnother != false)){
-				questionCount = questionCount + 1;
-				System.out.println("Question #" + questionCount + " was created successfully.");
-				
-				System.out.println("Question as reader would see it: ");
-				System.out.println("");
-				System.out.println("-- " + questions.get(questions.size()-1).getQuestionType() + " --");
-				System.out.println("");
-				// ****(MULTI-THREADING WARNING) LINE WILL NOT BE CORRECT IN CASE OF A RACE CONDITION****
-				questions.get(questions.size()-1).displayQuestion();
-				
-			}
-			
-		} // while(addAnother)
-	} // createSurvey()
-	
-	public int getNumberOfQuestions(){
+	public int getNumberOfQuestions() {
 		return questions.size();
 	}
-	
+
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public void setQuestions(ArrayList<Question> questions) {
+		this.questions = questions;
+	}
+
+	public ArrayList<Question> getQuestions() {
+		return this.questions;
+	}
 	
+	public void addQuestion(Question question){
+		this.questions.add(question);
+	}
+	
+	public void displayQuestions() {
+		for (Question q : this.questions) {
+			q.displayQuestion();
+		}
+	}
+
 	/**
 	 * Saves the survey questions in a filenamed [survey_name].q
+	 * 
 	 * @throws IOException
 	 */
-	public void save() throws IOException{
-	
+	public void save() throws FileNotFoundException {
+
 		try {
-			FileOutputStream fileOut = new FileOutputStream("saves/survey/" + this.getName() + ".q");
+			FileOutputStream fileOut = new FileOutputStream("saves/survey/"
+					+ this.getName() + ".q");
 			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			out.writeObject(this.questions);
+			out.writeObject(this.getQuestions());
 			out.close();
 			fileOut.close();
-			
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			// I/O error
 			e.printStackTrace();
 		}
-	
+	}
 
+	/**
+	 * Loads a survey from .q file located in saves/survey/
+	 * 
+	 * @param filename
+	 *            name of survey to load
+	 * @throws FileNotFoundException
+	 */
+	@SuppressWarnings("unchecked")
+	public void load(String filename) throws FileNotFoundException {
+		try {
+			FileInputStream fileIn = new FileInputStream("saves/survey/"+ filename);
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			// try reading object from file
+			try {
+				this.setQuestions((ArrayList<Question>) in.readObject());
+				this.setName(filename);
+			} catch (ClassNotFoundException e) {
+				// bad input
+				//e.printStackTrace();
+				System.out.println("Bad input");
+			}
+			in.close();
+		} catch (IOException e) {
+			// I/O error
+			// e.printStackTrace();
+			System.out.println("Couldn't find the survey your looking for. Try \\list");
+		}
 	}
 
 }
