5707e1b29280381c8cfe9c2fb27a7cbf3642f017
Sunny Patel <du.svp28@gmail.com>
2013-10-21 13:47:58 -0400

    created make file and inputs dir for all sorting algorithms

--- Senior Yr/Fall/cs457/ProgrammingAssignment1/insertion/insertion.cpp
+++ Senior Yr/Fall/cs457/ProgrammingAssignment1/insertion/insertion.cpp
@@ -1,9 +1,19 @@
+// my first program in C++
+
 #include <iostream>
 
 using namespace std;
 
-int main()
+int main (int argc, char *argv[])
 {
-	cout<<"Hey";
-	cin.get();
+	if(argc < 2){
+	  cout << "Not enough input params!\n";
+		return 0;
+	}
+
+	string inputFile = argv[0];
+	string outputFile = argv[1];
+
+	cout << inputFile << "\n";
+	cout << outputFile << "\n";
 }
