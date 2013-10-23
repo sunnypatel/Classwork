229eaedd418fe8dd1930e5f45c33ecdcec14440a
sunnypatel <sunnypatel@nc-118-234.dhcp.drexel.edu>
2013-10-22 22:36:01 -0400

    starting to add testing files for alg & data structs hw

--- Senior Yr/Fall/cs457/ProgrammingAssignment1/insertion/insertion.cpp
+++ Senior Yr/Fall/cs457/ProgrammingAssignment1/insertion/insertion.cpp
@@ -3,13 +3,17 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
+#include <sys/time.h>
 
 using namespace std;
+
+void printArray(int array[], int size);
+static long getTime();
 
 int main (int argc, char *argv[])
 {
 	char* inputFile = argv[1];
-	string outputFile = argv[2];
+	char* outputFile = argv[2];
 
 	cout << "inputFile=" << inputFile << "\n";
 	cout << "outputFile=" << outputFile << "\n";
@@ -48,7 +52,70 @@ int main (int argc, char *argv[])
 		cout << "Unable to open file";
 	}
 
+	// Before sorting
+	//cout << "Before sorting" << endl;
+	//printArray(array,arraySize);
+
+	int startTime = getTime();
+
+	// INSERTION SORT
+	int i,j,tmp;
+	for( i=1; i<arraySize; i++){
+		j = i;
+		while( j > 0 && (array[j-1] > array[j]) ){
+			
+			tmp = array[j];
+			array[j] = array[j-1];
+			array[j-1] = tmp;
+			j--;
+			
+		}
+	}
+	int endTime = getTime();
+	
+	printf("Time elapsed:  %ld \n", endTime - startTime);
+	
+	//cout << "After sorting" << endl;
+	//printArray(array, arraySize);
+
+	// Write to output file
+	std::ofstream outFile;
+	outFile.open(outputFile, std::ofstream::out);
+	if(outFile.is_open()){
+		outFile << "# Size of input array:\n";
+		outFile << inputSize << "\n";
+		outFile << "# Elements of the array:\n";
+
+		for(i=0; i<arraySize; i++){
+			outFile << array[i] << "\n";
+		}		
+
+	} else {
+		cout << "Could not open output file!" << endl;
+	}
+
 	return 0;
 }
 
+void printArray(int array[], int size){
+	int i;
+	cout << " Array: " << endl;
+	for(i=0; i<size; i++){
+		cout << " " << array[i];
+	}
+	cout << endl;
+}
 
+static long getTime() 
+{ 
+ long timeMilliseconds; 
+ struct timeval time_data; /* seconds since 0 GMT */ 
+ 
+ gettimeofday(&time_data,NULL); 
+ 
+ timeMilliseconds = time_data.tv_usec; 
+ timeMilliseconds /= 1000; 
+ timeMilliseconds += time_data.tv_sec * 1000 ; 
+ 
+ return timeMilliseconds; 
+} 
