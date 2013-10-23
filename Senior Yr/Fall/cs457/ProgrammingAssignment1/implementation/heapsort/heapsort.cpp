b16e14f5d45ebf7d9184a71961da9ee2d0525f27
Sunny Patel <du.svp28@gmail.com>
2013-10-23 13:39:40 -0400

    Finished the implementation of heapsort need to start testing and creating reports for heapsort testing

---
+++ Senior Yr/Fall/cs457/ProgrammingAssignment1/implementation/heapsort/heapsort.cpp
@@ -0,0 +1,181 @@
+// my first program in C++
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <sys/time.h>
+
+using namespace std;
+
+void printArray(int array[], int size);
+static long getTime();
+
+void heapify(int* array, int low, int high);
+void heapSort(int* array, int size);
+void shiftRight(int* array, int low, int high);
+
+int main (int argc, char *argv[])
+{
+	char* inputFile = argv[1];
+	char* outputFile = argv[2];
+
+	cout << "inputFile=" << inputFile << "\n";
+	cout << "outputFile=" << outputFile << "\n";
+
+	string line;
+	string inputSize;
+	int arraySize;
+
+	std::ifstream ifs;
+	ifs.open (inputFile, std::ifstream::in);
+	// ignore the first line
+	getline(ifs,line);
+	// get the input array size
+	getline(ifs,inputSize);
+	// ignore the 3rd line
+	getline(ifs,line);
+	// convert inputSize from string to int
+	arraySize = atoi(inputSize.c_str());	
+	// create an array with inputSize
+	int array[arraySize];
+	// print out the input size	
+	cout << "Input size= " << inputSize << endl;
+	
+	// if file was able to open
+	if(ifs.is_open()){
+		int i=0;
+		// open through and add inputs to array	
+		while ( getline(ifs,line) ){
+			// convert input line from string to int and 
+			// save in array
+			array[i] = atoi(line.c_str());
+			i++;
+		}
+		ifs.close();
+	} else{
+		cout << "Unable to open file";
+	}
+
+	// Before sorting
+	//cout << "Before sorting" << endl;
+	//printArray(array,arraySize);
+
+	int startTime = getTime();
+	int temp[arraySize];
+	// 	HEAPSORT
+	heapSort(array, arraySize-1);
+	
+	int endTime = getTime();
+	
+	printf("Time elapsed:  %ld \n", endTime - startTime);
+	
+
+	//cout << "After sorting" << endl;
+	//printArray(array, arraySize);
+
+	// Write to output file
+	std::ofstream outFile;
+	outFile.open(outputFile, std::ofstream::out);
+	if(outFile.is_open()){
+		outFile << "# Size of input array:\n";
+		outFile << inputSize << "\n";
+		outFile << "# Elements of the array:\n";
+		int i;
+		for(i=0; i<arraySize; i++){
+			outFile << array[i] << "\n";
+		}		
+
+	} else {
+		cout << "Could not open output file!" << endl;
+	}
+
+
+	FILE *pfile;
+	pfile = fopen("Analytics.txt","a");
+	fprintf(pfile, "Time elapsed: %ld \n", endTime - startTime);
+
+	return 0;
+}
+
+void printArray(int array[], int size){
+	int i;
+	cout << " Array: " << endl;
+	for(i=0; i<size; i++){
+		cout << " " << array[i];
+	}
+	cout << endl;
+}
+
+static long getTime() 
+{ 
+ long timeMilliseconds; 
+ struct timeval time_data; /* seconds since 0 GMT */ 
+ 
+ gettimeofday(&time_data,NULL); 
+ 
+ timeMilliseconds = time_data.tv_usec; 
+ timeMilliseconds /= 1000; 
+ timeMilliseconds += time_data.tv_sec * 1000 ; 
+ 
+ return timeMilliseconds; 
+} 
+
+void heapSort(int* array, int size){
+    assert(array);
+    assert(size > 0);
+    // move max value to root
+    heapify(array, 0, size-1);
+    int high = size - 1;
+    while (high > 0) {
+        
+        int tmp = array[high];
+        array[high] = array[0];
+        array[0] = tmp;
+        --high;
+        
+        shiftRight(array, 0, high);
+    }
+    return;
+}
+void heapify(int* array, int low, int high) {
+    // choose the last parent node
+    int midIdx = (high - low -1)/2;
+    while (midIdx >= 0) {
+        shiftRight(array, midIdx, high);
+        --midIdx;
+    }
+    return;
+}
+void shiftRight(int* array, int low, int high)
+{
+    int root = low;
+
+    while ( (root*2)+1 <= high )
+    {
+        int leftChild = (root * 2) + 1;
+        int rightChild = leftChild + 1;
+        int swapI = root;
+
+        // Left check
+        if (array[swapI] < array[leftChild])
+        {
+            swapI = leftChild;
+        }
+        // Right check
+        if ( (rightChild <= high) && (array[swapI] < array[rightChild]) ) {
+            swapI = rightChild;
+        }
+        
+        if (swapI != root){
+            int tmp = array[root];
+            array[root] = array[swapI];
+            array[swapI] = tmp;
+            
+            root = swapI;
+        }
+        else{
+            break;
+        }
+    }
+    return;
+}
