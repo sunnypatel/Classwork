8644b443c31d95eb96e5ca7ab792bc0e6d39f72c
Sunny Patel <du.svp28@gmail.com>
2013-11-14 22:31:31 -0500

    added research object detection research paper

--- Senior Yr/Fall/cs380/Assignments/Assignment4/src/smarterOthello/smartAss.java
+++ Senior Yr/Fall/cs380/Assignments/Assignment4/src/smarterOthello/smartAss.java
@@ -18,4 +18,46 @@ public class smartAss extends cs380.othello.OthelloPlayer {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    public OthelloMove minimaxDecision(OthelloState state){
+        return null;
+    }
+    /**
+     * Recrusively traverse tree all the way down to the leaves,
+     * to determine the backed-up value of a state
+     * @param state
+     * @return 
+     */
+    public int maxValue(OthelloState state){
+        if(state.gameOver()){
+            return state.score();
+        }
+        
+        // set some sample score for now as basis
+        int v = state.applyMoveCloning(state.generateMoves().get(0)).score();
+        
+        for(OthelloMove a : state.generateMoves()){
+            int u = minValue(state.applyMoveCloning(a));
+            if(u >= v){
+                v = u;
+            }
+        }
+        return v;
+    }
+    
+    public int minValue(OthelloState state){
+        if(state.gameOver()) return state.score();
+        
+        // set some sample score for now as basis
+        int v = state.applyMoveCloning(state.generateMoves().get(0)).score();
+        
+        for(OthelloMove a : state.generateMoves()){
+            int u = maxValue(state.applyMoveCloning(a));
+            if(u <= v){
+                v = u;
+            }
+        }
+        return v;
+    }
+    
+    
 }
